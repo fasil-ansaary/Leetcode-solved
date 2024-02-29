5964068718901d926b202119605a492b69b753a9
Fasil Ansaary <90245730+fasil-ansaary@users.noreply.github.com>
2024-02-29 15:07:45 +0530

    Time: 17 ms (62.42%) | Memory: 11.9 MB (38.31%) - LeetSync

---
+++ Google/43-multiply-strings/multiply-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        v1 = int(num1)
+        v2 = int(num2)
+        v3 = v1*v2
+        return str(v3)
